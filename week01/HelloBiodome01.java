7ce3c3949fc82c8f7166f0001fe4b619f7c5677c
cat hanbit <test@gmail.com>
2025-05-08 11:01:01 +0900

    1주차 등록

---
+++ week01/HelloBiodome01.java
@@ -0,0 +1,16 @@
+package week01;
+
+public class HelloBiodome01 {
+    public static void main(String[] args) {
+         if (args.length>0){
+            String name = args[0];
+            System.out.printf("안녕하세요. %s님 환영합니다.",name);
+             
+        }
+        else {
+            System.out.println("재입력하세요");
+        }
+
+    }
+}
+
