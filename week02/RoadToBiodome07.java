4fcb045b68fbe0860a4fb4d02c90c72e3b771eeb
cat hanbit <test@gmail.com>
2025-05-25 21:23:12 +0900

    1과 모두 완료 2-7 해야함

--- week02/RoadToBiodome07.java
+++ week02/RoadToBiodome07.java
@@ -1,55 +1,22 @@
 package week02;
 
-import java.util.Objects;
+import java.security.SecureRandom;
 
-public class RoadToBiodome07 {
+/*public class RoadToBiodome07 {
     public static void main(String[] args) {
-        int n = args.length;
-        String[] arr = new String[n];
-        String[] count = new String[n];
-        int[] cntArr = new int[n];
-        for (int i = 0; i < n; i++) {
-            if (args[i].matches("\\d+")) {
-                System.out.println("숫자는 입력받지 않습니다..");
-                return;
-            }
-            arr[i] = args[i];
-            count[i] = arr[i];
-            cntArr[i] = 1;
-        }
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (Objects.equals(arr[i], arr[j])) {
-                    cntArr[i]++;
-                    arr[j] = "";
+        if (args.length > 0) {
+            // args[0]에 쉼표로 구분된 문자열이 들어있다고 가정
+            String input = args[0];
+            String[] items = input.split(",");
+            Object [][] temp= {{}};
+
+                        i++;
+                    }
                 }
             }
-            if (!arr[i].isEmpty()) count[i] = count[i] + cntArr[i];
-            else count[i] = "";
+        } else {
+            System.out.println("입력값이 없습니다.");
         }
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                int numI = cnt_Number(count[i]);
-                int numJ = cnt_Number(count[j]);
-                if (numI < numJ) {
-                    String temp = count[i];
-                    count[i] = count[j];
-                    count[j] = temp;
-                }
-            }
-        }
-        for (String s : count) {
-            if (!s.isEmpty()) System.out.print(cnt_lWord(s)+" ");
-        }
-    }
-
-    public static int cnt_Number(String s) {
-        String number = s.replaceAll("[^\\d]", "");
-        if (number.isEmpty()) return 0;
-        return Integer.parseInt(number);
-    }
-
-    public static String cnt_lWord(String s) {
-        return s.replaceAll("\\d", "");
     }
 }
+
