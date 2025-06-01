d39822e01d5f264e29384ff5ec9a373e188830a9
cat hanbit <test@gmail.com>
2025-06-01 18:37:11 +0900

    2-7 완료

--- week02/RoadToBiodome07.java
+++ week02/RoadToBiodome07.java
@@ -1,22 +1,62 @@
 package week02;
 
-import java.security.SecureRandom;
+import java.util.*;
 
-/*public class RoadToBiodome07 {
+public class RoadToBiodome07 {
     public static void main(String[] args) {
-        if (args.length > 0) {
-            // args[0]에 쉼표로 구분된 문자열이 들어있다고 가정
-            String input = args[0];
-            String[] items = input.split(",");
-            Object [][] temp= {{}};
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        input=input.replaceAll("[^가-힣,]", "");
+        String[] arr = input.split(",");
+        Map<String, Integer> freq = new HashMap<>();
+        counting(arr,freq);
+        List<Map.Entry<String, Integer>> entryList = new ArrayList<>(freq.entrySet());
+        sort(entryList);
+        print(entryList);
+        sc.close();
+        }
 
-                        i++;
-                    }
+
+    public static void sort(List<Map.Entry<String, Integer>> entryList) {
+        int n = entryList.size();
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                // 두 Map.Entry 객체 비교 로직
+                // compareEntries 메서드를 통해 커스텀 정렬 기준을 적용
+                if (compare(entryList.get(j), entryList.get(minIndex)) < 0) {
+                    minIndex = j;
                 }
             }
-        } else {
-            System.out.println("입력값이 없습니다.");
+            // 가장 "작은" (즉, 가장 높은 우선순위) 요소를 찾았으면 현재 위치(i)와 교환
+            Map.Entry<String, Integer> temp = entryList.get(i);
+            entryList.set(i, entryList.get(minIndex));
+            entryList.set(minIndex, temp);
         }
+    }
+    public static int compare(Map.Entry<String, Integer> e1, Map.Entry<String, Integer> e2) {
+       int freqcompare=e2.getValue().compareTo(e1.getValue());
+       if(freqcompare==0){
+           return e1.getKey().compareTo(e2.getKey());
+       }
+       else{
+           return freqcompare;
+       }
+    }
+    public static void counting(String[] arr,Map<String,Integer> freq){
+        for (String s : arr) {
+            s = s.trim(); // 혹시 모를 공백 제거
+            if (!s.isEmpty()) { // 빈 문자열은 스킵
+                freq.put(s, freq.getOrDefault(s, 0) + 1);
+            }
+        }
+    }
+    public static void print(List<Map.Entry<String, Integer>> entryList) {
+        List<String> result = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : entryList) {
+            result.add(entry.getKey());
+        }
+        System.out.println(String.join(",", result));
     }
 }
 
